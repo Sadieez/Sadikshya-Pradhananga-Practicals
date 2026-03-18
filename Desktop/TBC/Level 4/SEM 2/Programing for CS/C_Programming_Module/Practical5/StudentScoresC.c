9f0475ca44b9086d8fa1956cd83c94d14ab48160
Sadieez <prsadikshya24@tbc.edu.np>
2026-03-18 23:01:03 +0545

    Added Practical 5 files

---
+++ Desktop/TBC/Level 4/SEM 2/Programing for CS/C_Programming_Module/Practical5/StudentScoresC.c
@@ -0,0 +1,58 @@
+/* StudentScoresB.c
+   Practical 6, Part 1 (C)
+   Using constants for array size
+   Sadikshya Pradhananga */
+
+   #include <stdio.h>
+   #define NUM_STUDENTS 10
+
+   int main() {
+    int scores[NUM_STUDENTS];
+    int sum = 0;
+    double average;
+
+    printf("Enter %d student scores:\n", NUM_STUDENTS);
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        printf("Score %d: ", i + 1);
+        scanf("%d", &scores[i]);
+     }
+   
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        sum += scores[i];
+    }
+
+    average = (double)sum / NUM_STUDENTS;
+    // Find maximum score
+     int maxScore = scores[0];
+     int maxIndex = 0;
+     for (int i = 1; i < NUM_STUDENTS; i++) {
+     if (scores[i] > maxScore) {
+        maxScore = scores[i];
+        maxIndex = i;
+    }
+}
+
+     // Find minimum score
+     int minScore = scores[0];
+     int minIndex = 0;
+     for (int i = 1; i < NUM_STUDENTS; i++) {
+     if (scores[i] < minScore) {
+        minScore = scores[i];
+        minIndex = i;
+    }
+}
+
+    printf("\nIndividual Scores:\n ");
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        printf("Student %d: %d\n", i + 1, scores[i]);
+    }
+
+    printf("\nSum: %d\n", sum);
+    printf("Average: %.21f\n", average);
+
+    printf("\nStatistics:\n");
+    printf("Highest score: %d (Student %d)\n", maxScore, maxIndex + 1);
+    printf("Lowest score: %d (Student %d)\n", minScore, minIndex + 1);
+
+    return 0;
+}
